544a8fc886c375228b5a160bfa1768cf01345644
namark <namark@disroot.org>
2018-11-23 04:33:43 +0400

    Prototype simple vector graphics library.

---
+++ simple_vg.cpp
@@ -0,0 +1,147 @@
+#include "simple_vg.h"
+#include "simple/support/enum.hpp"
+
+using namespace simple::vg;
+
+canvas::canvas(flags f) noexcept :
+#if defined NANOVG_GL2
+	raw(nvgCreateGL2(support::to_integer(f)))
+#elif defined NANOVG_GL3
+	raw(nvgCreateGL3(support::to_integer(f)))
+#elif defined NANOVG_GLES2
+	raw(nvgCreateGLES2(support::to_integer(f)))
+#elif defined NANOVG_GLES3
+	raw(nvgCreateGLES3(support::to_integer(f)))
+#endif
+{
+}
+
+void canvas::deleter::operator()(NVGcontext* context) const noexcept
+{
+#if defined NANOVG_GL2
+	nvgDeleteGL2(context);
+#elif defined NANOVG_GL3
+	nvgDeleteGL3(context);
+#elif defined NANOVG_GLES2
+	nvgDeleteGLES2(context);
+#elif defined NANOVG_GLES3
+	nvgDeleteGLES3(context);
+#endif
+}
+
+canvas& canvas::clear(const rgba_vector& color) noexcept
+{
+	glClearColor(color.r(),color.g(),color.b(),color.a());
+	glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+	return *this;
+}
+
+frame canvas::begin_frame(float2 size, float pixelRatio) noexcept
+{
+	return frame(raw.get(), size, pixelRatio);
+}
+
+frame::frame(NVGcontext* context, float2 size, float pixelRatio) noexcept :
+	context(context),
+	size(size),
+	pixelRatio(pixelRatio)
+{
+	nvgBeginFrame(context, size.x(), size.y(), pixelRatio);
+}
+
+frame::~frame() noexcept
+{
+	nvgEndFrame(context);
+}
+
+sketch frame::begin_sketch() noexcept
+{
+	return sketch(context);
+}
+
+sketch::sketch(NVGcontext* context) noexcept :
+	context(context)
+{
+	nvgSave(context);
+	nvgBeginPath(context);
+}
+
+sketch::~sketch() noexcept
+{
+	nvgRestore(context);
+}
+
+void ellipse(NVGcontext* context, const float2& center, const float2& radius) noexcept
+{
+	nvgEllipse(context, center.x(), center.y(), radius.x(), radius.y());
+}
+
+void rectangle(NVGcontext* context, const float2& position, const float2& size) noexcept
+{
+	nvgRect(context, position.x(), position.y(), size.x(), size.y());
+}
+
+sketch& sketch::ellipse(const range2f& bounds) noexcept
+{
+	const auto& radius = (bounds.upper() - bounds.lower())/2;
+	const auto& center = bounds.lower() + radius;
+	::ellipse(context, center, radius);
+	return *this;
+}
+
+sketch& sketch::rectangle(const range2f& bounds) noexcept
+{
+	const auto& position = bounds.lower();
+	const auto& size = bounds.upper() - bounds.lower();
+	::rectangle(context, position, size);
+	return *this;
+}
+
+sketch& sketch::fill(const rgba_vector& color) noexcept
+{
+	nvgFillColor(context, nvgRGBAf(color.r(), color.g(), color.b(), color.a()));
+	return fill();
+}
+
+sketch& sketch::fill() noexcept
+{
+	nvgFill(context);
+	return *this;
+}
+
+sketch& sketch::line_cap(cap c) noexcept
+{
+	nvgLineCap(context, support::to_integer(c));
+	return *this;
+}
+
+sketch& sketch::line_join(join j) noexcept
+{
+	nvgLineJoin(context, support::to_integer(j));
+	return *this;
+}
+
+sketch& sketch::line_width(float width) noexcept
+{
+	nvgStrokeWidth(context, width);
+	return *this;
+}
+
+sketch& sketch::miter_limit(float limit) noexcept
+{
+	nvgMiterLimit(context, limit);
+	return *this;
+}
+
+sketch& sketch::outline(const rgba_vector& color) noexcept
+{
+	nvgStrokeColor(context, nvgRGBAf(color.r(), color.g(), color.b(), color.a()));
+	return outline();
+}
+
+sketch& sketch::outline() noexcept
+{
+	nvgStroke(context);
+	return *this;
+}
+
---
+++ simple_vg.h
@@ -0,0 +1,106 @@
+#include <memory>
+#include "nanovg_full.h"
+#include "simple/support/enum_flags_operators.hpp"
+#include "simple/geom/vector.hpp"
+#include "simple/graphical/color_vector.hpp"
+#include "simple/graphical/common_def.h"
+
+namespace simple::vg
+{
+	using float2 = geom::vector<float, 2>;
+	using graphical::rgb_vector;
+	using graphical::rgba_vector;
+	using graphical::rgb_pixel;
+	using graphical::rgba_pixel;
+	using range2f = support::range<float2>;
+	using rect2f = geom::segment<float2>;
+	using anchored_rect2f = geom::anchored_segment<float2>;
+
+	class frame;
+
+	class canvas
+	{
+		public:
+
+		enum class flags
+		{
+			nothing = 0,
+			antialias = NVG_ANTIALIAS,
+			stencil_strokes = NVG_STENCIL_STROKES,
+			debug = NVG_DEBUG
+		};
+
+		canvas(flags) noexcept;
+
+		canvas& clear(const rgba_vector& color = rgba_vector::white()) noexcept;
+
+		frame begin_frame(float2 size, float pixelRatio = 1) noexcept;
+
+		private:
+
+		struct deleter
+		{
+			void operator()(NVGcontext* context) const noexcept;
+		};
+
+		std::unique_ptr<NVGcontext, deleter> raw;
+	};
+	using ::operator |;
+	using ::operator &;
+	using ::operator &&;
+
+	class sketch;
+
+	class frame
+	{
+		public:
+			sketch begin_sketch() noexcept;
+			~frame() noexcept;
+		private:
+			NVGcontext* context;
+			float2 size;
+			float pixelRatio;
+			frame(NVGcontext*, float2 size, float pixelRatio = 1) noexcept;
+			friend class canvas;
+	};
+
+	class sketch
+	{
+		public:
+			enum class cap
+			{
+				butt = NVG_BUTT,
+				round = NVG_ROUND,
+				square = NVG_SQUARE
+			};
+
+			enum class join
+			{
+				miter = NVG_MITER,
+				round = NVG_ROUND,
+				bevel = NVG_BEVEL
+			};
+
+			sketch& ellipse(const range2f&) noexcept;
+			sketch& rectangle(const range2f&) noexcept;
+
+			sketch& fill(const rgba_vector& color) noexcept;
+			sketch& fill() noexcept;
+			sketch& line_cap(cap) noexcept;
+			sketch& line_join(join) noexcept;
+			sketch& line_width(float) noexcept;
+			sketch& miter_limit(float) noexcept;
+			sketch& outline(const rgba_vector& color) noexcept;
+			sketch& outline() noexcept;
+
+			~sketch() noexcept;
+		private:
+			NVGcontext* context;
+			sketch(NVGcontext*) noexcept;
+			friend class frame;
+	};
+
+} // namespace simple::vg
+
+template<> struct simple::support::define_enum_flags_operators<simple::vg::canvas::flags>
+	: std::true_type {};
